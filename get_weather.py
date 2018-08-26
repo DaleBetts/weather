88a599493120dc26b9cd9908b66ca134ab6ec6df
Dale Betts <dale.betts@ubertasconsulting.com>
2018-08-26 20:52:32 +0100

    initial push

---
+++ get_weather.py
@@ -0,0 +1,27 @@
+# This will fetch the weather. 
+
+import requests, json, sys, argparse
+
+
+def get_weather_for_city(url,city_id,api_key):
+    result = ""
+    try:
+        r = requests.get(url + "?id=" + city_id + "&APPID=" + api_key)
+        if r.status_code != 200:
+            print('Error fetching weather from openweathermap.org, status code:' + r.status_code)
+        result = r.text
+    except Exception as e:
+        print(e)
+    return result
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--cityid', default='london', help='The city to fetch the weather for', dest="city_id")
+parser.add_argument('--apikey', default='123456', help='Your API Key for openweathermap.org', dest="api_key")
+args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
+api_key = args.api_key
+city_id = args.city_id
+
+city_input = get_weather_for_city("http://api.openweathermap.org/data/2.5/forecast",city_id,api_key)
+print(city_input)
+
